2ed67977cb27b266dfebe8516b229d99eb6a4512
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-04-24 16:58:45 +0300

    Game Logic

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const secretnumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretnumber;
+
+let score=20;
+document.querySelector('.score').textContent=score;
+
 document.querySelector('.check').addEventListener(
     'click', function(){
        const guess = Number(document.querySelector('.guess').value);
@@ -8,7 +14,44 @@ document.querySelector('.check').addEventListener(
 
        if(!guess)
        {
-        document.querySelector('.message').textContent='⛔ No Number'
+        document.querySelector('.message').textContent='⛔ No Number!';
+        score--;
+        document.querySelector('.score').textContent=score;
+       }
+
+       else if (secretnumber === guess)
+       {
+        document.querySelector('.message').textContent='🎉 Correct Number!';
+       }
+       else if (guess > secretnumber)
+       {
+        if(score>1)
+        {
+            document.querySelector('.message').textContent='📈 Too High!';
+            score--;
+            document.querySelector('.score').textContent=score;
+        }
+        else
+        {
+            document.querySelector('.message').textContent='💥 You lost the game!';
+            document.querySelector('.score').textContent=0;
+        }
+        
+       }
+       else if (guess < secretnumber)
+       {
+        if(score>1)
+        {
+            document.querySelector('.message').textContent='📉 Too Low!';
+            score--;
+            document.querySelector('.score').textContent=score;
+        }
+        else
+        {
+            document.querySelector('.message').textContent='💥 You lost the game!';
+            document.querySelector('.score').textContent=0;
+        }
+       
        }
     }
 )
