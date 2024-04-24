c75e4397a0e5cb25fc61cf9e3cbcf90e7a0e78ea
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-04-24 16:29:21 +0300

    Some Dome manipulations

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+document.querySelector('.check').addEventListener(
+    'click', function(){
+       const guess = Number(document.querySelector('.guess').value);
+
+       console.log(guess, typeof guess);
+
+       if(!guess)
+       {
+        document.querySelector('.message').textContent='⛔ No Number'
+       }
+    }
+)
